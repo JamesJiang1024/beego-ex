c1222d182e65d45268cb73db2f3b7daf209f2e54
wentian <jwentian@redhat.com>
2017-03-01 13:43:20 +0800

    add script

--- hack/buildimage.sh
+++ hack/buildimage.sh
@@ -3,6 +3,4 @@ version=`git rev-parse HEAD | cut -b 1-6`
 rm -rf .output/*
 bee pack
 mv beego-ex.tar.gz .output/
-tar -zxvf .output/beego-ex.tar.gz -C .output/
-rm -rf .output/beego-ex.tar.gz
 sudo docker build -t beego-ex:v1-$version .
--- hack/pushimage.sh
+++ hack/pushimage.sh
@@ -6,6 +6,7 @@ version=v1-`git rev-parse HEAD | cut -b 1-6`
 echo "Save And Push Images"
 sudo docker save beego-ex:$version > .output/beego-ex.tar
 scp .output/beego-ex.tar root@10.221.130.221:/root/
+rm -rf .output/beego-ex.tar
 ssh root@10.221.130.221 "docker load < beego-ex.tar"
 ssh root@10.221.130.221 "docker tag beego-ex:$version registry.travelsky.com/beego/beego-ex:$version"
 ssh root@10.221.130.221 "docker push registry.travelsky.com/beego/beego-ex:$version"
